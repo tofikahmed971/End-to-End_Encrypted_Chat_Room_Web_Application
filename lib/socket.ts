44430326bb6c0775cb0ca77f345fa3ecf7799285
Mohammad Shahid <shahid0mohmmad0@gmail.com>
2025-11-26 15:18:26 +0530

    feat: Initialize Socket.IO client with autoConnect disabled

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3001";
+const URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:8000";
 
 export const socket = io(URL, {
     autoConnect: false,
