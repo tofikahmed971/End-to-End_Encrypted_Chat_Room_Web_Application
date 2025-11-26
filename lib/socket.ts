6c21f4ba402357824d5e8832b7ddbda8a51b9b77
Mohammad Shahid <shahid0mohmmad0@gmail.com>
2025-11-26 17:53:21 +0530

    feat: add socket.io client utility for real-time communication

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:8000";
+const URL = process.env.NEXT_PUBLIC_SOCKET_URL || undefined;
 
 export const socket = io(URL, {
     autoConnect: false,
